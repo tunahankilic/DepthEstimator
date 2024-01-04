668e9c4ed9de3b492295f0ae89052153a148f5ac
Tunahan Kilic <kilic.tunahan@gmail.com>
2024-01-04 20:44:17 +0100

    add imgbb upload

--- api.py
+++ api.py
@@ -4,6 +4,7 @@ import uuid
 from fastapi import FastAPI, File, UploadFile
 
 from predictor import DepthEstimationModel
+from upload import upload_image_to_imgbb
 
 app = FastAPI()
 depth_estimator = DepthEstimationModel()
@@ -28,7 +29,8 @@ async def predict(file: UploadFile = File(...)):
             image_data.write(file.file.read())
 
         depth_estimator.calculate_depth_map(destination_path, output_path)
-        return {"OK": "Image has been saved."}
+        response = upload_image_to_imgbb(output_path)
+        return response
 
     except Exception as e:
         return {"error": str(e)}
